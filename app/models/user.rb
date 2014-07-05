eefa7bd5c82ff905950ffd6f15a8e3b9d72dddc8
StanislavBerkovich <stanislav.berkovich@gmail.com>
2014-07-05 12:15:15 +0300

    add rendered user panel

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
 
+  def is? request_role
+    self.role.to_s == request_role.to_s
+  end
 
   def role= role
     role.users << self
