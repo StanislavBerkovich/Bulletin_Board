5ccf0cf194166aff922ce6038ec7bb9228c8ae29
StanislavBerkovich <stanislav.berkovich@gmail.com>
2014-07-06 14:19:58 +0300

    add state in partial of advert

--- app/controllers/admin_job_controller.rb
+++ app/controllers/admin_job_controller.rb
@@ -1,4 +1,6 @@
 class AdminJobController < ApplicationController
+  before_filter :authenticate_user!
+
   def nonpublished
     @adverts = Advert.where(state: :new).page(params[:page]).per(10)
   end
--- app/helpers/adverts_helper.rb
+++ app/helpers/adverts_helper.rb
@@ -1,2 +1,16 @@
 module AdvertsHelper
+  def show_state advert
+    state = advert.state.to_sym
+    html = "<div class='alert alert-"
+    case state
+      when :new
+        html << "info"
+      when :rejected
+        html << 'error'
+      when :published
+        html << 'success'
+    end
+    html << "'>#{state}</div>"
+    html.html_safe
+  end
 end
