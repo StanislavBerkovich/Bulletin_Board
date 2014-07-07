79eb0068d551779dc89bf7a231fa6b5b934df408
StanislavBerkovich <stanislav.berkovich@gmail.com>
2014-07-07 20:07:23 +0300

    Refactoring of controllers" git push

--- app/controllers/admin_job_controller.rb
+++ app/controllers/admin_job_controller.rb
@@ -1,6 +1,6 @@
 class AdminJobController < ApplicationController
   before_filter :authenticate_user!
-
+  before_action :set_advert, only: [:approve, :rejected]
 
   rescue_from SQLite3::ConstraintException do |exception|
     flash[:error] = "Access denied."
@@ -12,17 +12,26 @@ class AdminJobController < ApplicationController
   end
 
   def approve
-    advert = Advert.find(params[:advert])
-    advert.state = :approved
-    advert.save
-    redirect_to :back
+    @advert.state = :approved
+    respond_to do |format|
+      if @advert.save
+        format.html { redirect_to :back, notice: 'Advert was successfully updated.' }
+      else
+        format.html { redirect_to :back, error: 'Something went wrong!' }
+      end
+    end
+
   end
 
   def rejected
-    advert = Advert.find(params[:advert])
-    advert.state = :rejected
-    advert.save
-    redirect_to :back
+    @advert.state = :rejected
+    respond_to do |format|
+      if @advert.save
+        format.html { redirect_to :back, notice: 'Advert was successfully updated.' }
+      else
+        format.html { redirect_to :back, error: 'Something went wrong!' }
+      end
+    end
   end
 
   def manage_advert_type
@@ -32,13 +41,23 @@ class AdminJobController < ApplicationController
 
   def create_type
     Type.new(params.require('type').permit(:name)).save
-    redirect_to :root
+    if Type.new(params.require('type').permit(:name)).save
+      format.html { redirect_to :back, notice: 'Type was successfully created.' }
+    else
+      format.html { redirect_to :back, error: 'Something went wrong!' }
+    end
   end
 
   def delete_type
     @type = Type.find(params[:id])
     @type.destroy
     redirect_to :back
+  end
+
+  private
+
+  def set_advert
+    @advert = Advert.find(params[:advert])
   end
 
 end
--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -18,6 +18,7 @@ class PersonsController < ApplicationController
     @person.email = input_params[:email] || @person.email
     @person.role = Role.find_by(name: input_params[:role]) || @person.role
     @person.password = input_params[:password] || @person.password
+
     @person.save
     redirect_to persons_profile_path id: @person
   end
