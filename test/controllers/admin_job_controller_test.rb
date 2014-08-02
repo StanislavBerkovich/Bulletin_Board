f825f8d147856d0690c786e705533c2a10f3237c
StanislavBerkovich <stanislav.berkovich@gmail.com>
2014-08-02 14:30:42 +0300

    Delete folder test

--- test/controllers/admin_job_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class AdminJobControllerTest < ActionController::TestCase
-  test "should get nonpublished" do
-    get :nonpublished
-    assert_response :success
-  end
-
-end
