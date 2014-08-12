2f50889caccf0525ab1cf12197cf264065da5f92
StanislavBerkovich <stanislav.berkovich@gmail.com>
2014-08-12 15:41:59 +0300

    Add config in gitignore

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module BulletinBoard
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+
+
     config.generators do |g|
       g.test_framework :rspec
       g.stylesheets     false
